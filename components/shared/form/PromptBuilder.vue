d77599c0749ef7aa9227e8fe113566bdbd960bf1
Peter Sopko <petersopko@protonmail.com>
2022-09-23 11:33:19 +0200

    :tada: unstable drop no.1

---
+++ components/shared/form/PromptBuilder.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-field :label="$t(label)" horizontal>
+    <b-input maxlength="200" type="textarea"></b-input>
+  </b-field>
+  <!--  <b-field :label="$t(label)" horizontal>-->
+  <!--    <b-input-->
+  <!--      ref="input"-->
+  <!--      v-model="vValue"-->
+  <!--      :placeholder="placeholder"-->
+  <!--      :expanded="expanded"-->
+  <!--      :maxlength="maxlength"-->
+  <!--      :required="required"-->
+  <!--      :type="type"-->
+  <!--      @blur="hasFocus = false"-->
+  <!--      @focus="hasFocus = true" />-->
+  <!--    <template v-if="hasFocus && message" #message>-->
+  <!--      <transition name="fade">-->
+  <!--        <span class="has-text-primary is-italic">{{ message }}</span>-->
+  <!--      </transition>-->
+  <!--    </template>-->
+  <!--  </b-field>-->
+</template>
+
+<script lang="ts">
+import { Component, Prop, Ref, VModel, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class BasicInput extends Vue {
+  // Dev: make vValue required
+  @VModel({ type: String }) vValue!: string
+  @Prop({ type: String, required: true }) label!: string
+  @Prop({ type: String, required: true }) placeholder!: string
+  @Prop({ type: Boolean, default: false }) expanded!: boolean
+  @Prop({ type: String }) message!: string
+  @Prop({ type: String, required: false }) maxlength!: string
+  @Prop({ type: String, required: false }) type!: string
+  @Prop({ type: Boolean, default: false }) required!: boolean
+  @Ref('input') readonly input
+  protected hasFocus = false
+  public checkValidity() {
+    return this.input.checkHtml5Validity()
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ pages/unstable-diffusion.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <p class="title is-size-3">
+      {{ 'Unstable Diffusion' }}
+    </p>
+    <PromptBuilder
+      :label="$t('unstableDiffusion.promptInput.label')"
+      :placeholder="$t('unstableDiffusion.promptInput.placeholder')" />
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { exist } from '@/components/search/exist'
+
+@Component({
+  components: {
+    PromptBuilder: () => import('@/components/shared/form/PromptBuilder.vue'),
+    DisabledInput: () => import('@/components/shared/DisabledInput.vue'),
+  },
+})
+export default class Transform extends Vue {
+  protected url = ''
+  public mounted(): void {
+    exist(this.$route.query.url, (value) => (this.url = value))
+  }
+
+  layout() {
+    return 'centered-half-layout'
+  }
+}
+</script>
+
+<style scoped></style>
